fe40fe577afd049f4734ebc66ec41e05e42fb0df
yukina1230 <lost.laugh1230@gmail.com>
2021-04-15 07:03:22 +0000

    customerバリデーション

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,9 +3,11 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
          has_many :orders
          has_many :shipping_adresses
          has_many :cart_products
-         
+
+  validates :postal_code, presence: true
+  validates :address, presence: true
 end
