e69f2c99d7a8dea2f90619abcfa23ff838cc8973
moro0823 <basell.4rm.0823@outlook.com>
2021-04-16 06:43:31 +0000

    [FINISH] public_product index show

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,7 +1,10 @@
 class Public::ProductsController < ApplicationController
   def index
+    @products = Product.all.page(params[:page]).per(8)
   end
 
   def show
+    @product = Product.find(params[:id])
   end
+
 end
