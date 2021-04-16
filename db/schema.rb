149eb8329dad5b0f53298e9be66982818d9d50b0
rwrw96 <kazuki137@gmail.com>
2021-04-16 04:53:17 +0000

    cart_products costomer_id => customer_idに変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_15_001909) do
+ActiveRecord::Schema.define(version: 2021_04_16_045058) do
 
   create_table "admin_users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2021_04_15_001909) do
 
   create_table "cart_products", force: :cascade do |t|
     t.integer "product_id"
-    t.integer "costomer_id"
+    t.integer "customer_id"
     t.integer "cart_count"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
