942d6789efc8fae55ea04f6bdfca16ba379affe0
rwrw96 <kazuki137@gmail.com>
2021-04-16 07:25:29 +0000

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 2021_04_16_045058) do
     t.string "postal_code"
     t.string "address"
     t.string "phone"
-    t.boolean "is_deleted", default: false
+    t.boolean "is_deleted"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
