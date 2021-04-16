5b7682db67c6534ad79607103a8b4a14c9ba86fb
rwrw96 <kazuki137@gmail.com>
2021-04-16 08:27:21 +0000

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_15_001909) do
+ActiveRecord::Schema.define(version: 2021_04_16_045058) do
 
   create_table "admin_users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 2021_04_15_001909) do
     t.string "postal_code"
     t.string "address"
     t.string "phone"
-    t.boolean "is_deleted", default: false
+    t.boolean "is_deleted"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
