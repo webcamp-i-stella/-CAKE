228a3758463e7cad343665144f00f86a1dc88366
yukina1230 <lost.laugh1230@gmail.com>
2021-04-16 12:25:23 +0000

    shipping_address/index.new追加

--- app/controllers/public/shipping_addresses_controller.rb
+++ app/controllers/public/shipping_addresses_controller.rb
@@ -1,11 +1,16 @@
 class Public::ShippingAddressesController < ApplicationController
   def index
-  end
-
-  def edit
+    @shipping_address = ShippingAddress.new
+    @shipping_addresses = ShippingAddress.all
   end
 
   def create
+    @shipping_address = ShippingAddress.new(shipping_address_params)
+    @shipping_address.save!
+    redirect_to shipping_addresses_path
+  end
+
+  def edit
   end
 
   def update
@@ -13,4 +18,11 @@ class Public::ShippingAddressesController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def shipping_address_params
+    params.require(:shipping_address).permit(:postal_code, :address, :name)
+  end
+
 end
--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,3 +1,3 @@
 class ShippingAddress < ApplicationRecord
-    belongs_to :customer
+    belongs_to :customer, optional: true
 end
