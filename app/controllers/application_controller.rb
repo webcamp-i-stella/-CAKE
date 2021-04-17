bdab14a1a7574eca8f4e553999f58934837671f4
yukina1230 <lost.laugh1230@gmail.com>
2021-04-17 02:23:53 +0000

    ログイン・ログアウト後の遷移先、ヘッダー編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+
+ private
+
+  def after_sign_in_path_for(resource_or_scope)
+    if resource_or_scope.is_a?(AdminUser)
+        root_path #ログイン後は注文履歴一覧に遷移、admin_orderの変数が出来次第
+    else
+        root_path
+    end
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :admin_user
+        new_admin_user_session_path
+    else
+        root_path
+    end
+  end
 end
