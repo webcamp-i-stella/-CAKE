39dc1fe1ba0575a0196811babad3e3d093b75f2e
moro0823 <basell.4rm.0823@outlook.com>
2021-04-17 01:19:18 +0000

    [FINISH] genre show

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,6 +1,8 @@
 class Public::ProductsController < ApplicationController
   def index
-    @products = Product.all.page(params[:page]).per(8)
+    @products = Product.all
+    @products_page = Product.all.page(params[:page]).per(8)
+    @genres = Genre.all
   end
 
   def show
