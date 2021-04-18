dba32a4d920861e904e6d43ecf9478e46a4ad317
yukina1230 <lost.laugh1230@gmail.com>
2021-04-18 01:14:55 +0000

    admin_customer詳細・編集ページ追加

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -8,8 +8,18 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    customer = Customer.find(params[:id])
+    customer.update(admin_customer_params)
+    redirect_to admin_customer_path(customer)
+  end
+
+  private
+
+  def admin_customer_params
+    params.require(:customer).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :postal_code, :address, :phone, :email, :is_deleted)
   end
 end
