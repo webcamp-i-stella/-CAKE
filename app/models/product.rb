57c46a17ea677bbda9f7bec4a103b525ca5e5c81
rwrw96 <kazuki137@gmail.com>
2021-04-19 22:29:14 +0000

    fix product.rb optional: trueを追記, orders/new支払方法 現金=> 銀行振込に変更

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
     has_many :cart_products,dependent: :destroy
-    belongs_to :customer
+    belongs_to :customer, optional: true
     has_many :order_details
     belongs_to :genre
     attachment :image
