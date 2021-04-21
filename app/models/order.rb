b42e2eeb24ef68f849a8642e05f23a3a46ddb168
rwrw96 <kazuki137@gmail.com>
2021-04-21 03:23:42 +0000

    cart_productsデザイン修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
     belongs_to :customer, optional: true
-    has_many :order_details
+    has_many :order_details, dependent: :destroy
     accepts_nested_attributes_for :order_details
 
 
