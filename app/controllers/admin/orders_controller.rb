63ee96a4e3a147e7cd55165d49acac9f45180464
yukina1230 <lost.laugh1230@gmail.com>
2021-04-21 23:31:25 +0000

    admin会員一覧修正

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,20 +1,26 @@
 class Admin::OrdersController < ApplicationController
   def index
-    @orders = Order.all
+    case params[:order_sort]
+    when "0"
+      @orders = Order.all
+    when "1"
+      @customer = Customer.find_by(params[:customer_id])
+      @orders = @customer.orders
+    end
     @orders = Order.all.page(params[:page])
   end
-  
+
   def show
     @order = Order.find(params[:id])
     @orders = Order.all
   end
-  
+
   def update
     @order = Order.find(params[:id])
     @order.update(order_status_params)
     redirect_to admin_order_path(@order)
   end
-  
+
   private
     def order_status_params
       params.require(:order).permit(:order_status).merge(order_status: params[:order][:order_status].to_i)
