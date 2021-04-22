efa1e486f97111d26b900594fddcfa3c871c7ce6
rwrw96 <kazuki137@gmail.com>
2021-04-22 06:43:25 +0000

    製作ステータス実装

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -5,7 +5,21 @@ class Admin::OrderDetailsController < ApplicationController
   def update
     @order_detail = OrderDetail.find(params[:id])
     @order_detail.update(order_detail_params)
-    redirect_to admin_order_path(@order_detail.order.id)
+    
+    if @order_detail.production_status == "製作中"
+       @order_detail.order.order_status = "製作中"
+    elsif
+      @order_detail.production_status == "製作完了"
+        if OrderDetail.exists?(production_status: "製作中")
+           @order_detail.order.order_status = "製作中"
+        else
+          @order_detail.order.order_status = "発送準備中"
+        end
+    end
+    
+    @order_detail.save
+    @order_detail.order.save
+    redirect_to admin_order_path(@order_detail.order_id)
   end
   
   private
